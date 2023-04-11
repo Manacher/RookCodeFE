e5abc100b286e6f415e286cc6ea511a4db4bd47c
YnmtDJ <1624309321@qq.com>
2023-04-12 00:21:02 +0800

    feat: 查看问题，代码编写

---
+++ src/components/Problem.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class = "problemView">
+    <h3>P{{id}} {{title}}</h3>
+    <span style="color: lightgray">难度 </span><span :style="{color: diffColor}">{{difficulty}}</span>
+    <a-divider style="height: 1px; background-color: lightgray" />
+    <Editor
+        style="min-height: 700px"
+        v-model="content"
+        :defaultConfig="editorConfig"
+        :mode="mode"
+        @onCreated="handleCreated"
+    />
+  </div>
+</template>
+
+<script>
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
+
+import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import {Boot} from '@wangeditor/editor'
+import markdownModule from '@wangeditor/plugin-md'
+
+Boot.registerModule(markdownModule)
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Problem",
+  components: { Editor},
+  setup() {
+    // 编辑器实例，必须用 shallowRef
+    const editorRef = shallowRef()
+    // 编辑器，工具栏配置
+    const toolbarConfig = {}
+    const editorConfig = {readOnly :true, scroll: true}
+    // 题目内容
+    const id = "0000"  // 题号
+    const title = "A+B 问题"  // 题目标题
+    const difficulty = "简单"  // 题目难度
+    let diffColor = "#63ec0c"  // ”难度“颜色
+    const content = ref("<p>给你一个字符串 <code>s</code> ，请你统计并返回这个字符串中 <strong>回文子串</strong> 的数目。</p><p><strong>回文字符串</strong> 是正着读和倒过来读一样的字符串。</p>\n" +
+        "\n" +
+        "<p><strong>子字符串</strong> 是字符串中的由连续字符组成的一个序列。</p>\n" +
+        "\n" +
+        "<p>具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。</p>\n" +
+        "\n" +
+        "<p>&nbsp;</p>\n" +
+        "\n" +
+        "<p><strong>示例 1：</strong></p>\n" +
+        "\n" +
+        "<pre><strong>输入：</strong>s = \"abc\"\n" +
+        "<strong>输出：</strong>3\n" +
+        "<strong>解释：</strong>三个回文子串: \"a\", \"b\", \"c\"\n" +
+        "</pre>\n" +
+        "\n" +
+        "<p><strong>示例 2：</strong></p>\n" +
+        "\n" +
+        "<pre><strong>输入：</strong>s = \"aaa\"\n" +
+        "<strong>输出：</strong>6\n" +
+        "<strong>解释：</strong>6个回文子串: \"a\", \"a\", \"a\", \"aa\", \"aa\", \"aaa\"</pre>\n" +
+        "\n" +
+        "<p>&nbsp;</p>\n" +
+        "\n" +
+        "<p><strong>提示：</strong></p>\n" +
+        "\n" +
+        "<ul>\n" +
+        "\t<li><code>1 &lt;= s.length &lt;= 1000</code></li>\n" +
+        "\t<li><code>s</code> 由小写英文字母组成</li>\n" +
+        "</ul>")
+
+
+    // ajax 异步获取后端数据
+    onMounted(() => {
+      console.log("get data")
+
+    })
+
+    // 组件销毁时，也及时销毁编辑器
+    onBeforeUnmount(() => {
+      const editor = editorRef.value
+      if (editor == null) return
+      editor.destroy()
+    })
+
+    // 创建编辑器后
+    const handleCreated = (editor) => {
+      editorRef.value = editor // 记录 editor 实例，重要！
+
+    }
+
+    return {
+      editorRef,
+      id,
+      title,
+      difficulty,
+      diffColor,  // ”难度“字体颜色
+      content,
+      mode: 'default', // 或 'simple'
+      toolbarConfig,
+      editorConfig,
+      handleCreated
+    };
+  }
+
+}
+</script>
+
+<style scoped>
+  .problemView{
+    width: 500px;
+    overflow-y: hidden;
+    text-align: left
+  }
+
+
+</style>
---
+++ src/components/Submit.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="submitView">
+    <div class="code">
+      <codemirror
+          v-model="code"
+          placeholder="Your code here..."
+          :style="options.style"
+          :mode="options.mode"
+          :spellcheck="options.spellcheck"
+          :autofocus="options.autofocus"
+          :indent-with-tab="options.indentWithTab"
+          :tabSize="options.tabSize"
+          :extensions="options.extensions"
+          @ready="log('ready', $event)"
+          @change="log('change', $event)"
+          @focus="log('focus', $event)"
+          @blur="log('blur', $event)"
+      />
+    </div>
+    <div class="submit">
+      <a-space>
+        <a-select
+            ref="select"
+            v-model:value="language"
+            style="width: 100px"
+            @focus="focus"
+            @change="handleLangChange">
+          <a-select-option value="cpp">cpp</a-select-option>
+          <a-select-option value="c">c</a-select-option>
+          <a-select-option value="python">python</a-select-option>
+          <a-select-option value="java">java</a-select-option>
+        </a-select>
+        <a-button type="primary">提交</a-button>
+      </a-space>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Codemirror } from "vue-codemirror";
+import { cpp } from "@codemirror/lang-cpp";
+import { python } from "@codemirror/lang-python";
+import { java } from "@codemirror/lang-java";
+import { oneDark } from "@codemirror/theme-one-dark";
+
+import {reactive, ref} from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Submit",
+  components:{Codemirror,},
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  setup(){
+    const code = ref("");  // 用户编写的代码
+    let language = "cpp";  // 选择的代码语言
+    const options = reactive({
+      style: { height: "400px" },
+      mode: "text/x-c++src",
+      spellcheck: true,
+      autofocus: true,
+      indentWithTab: true,
+      tabSize: 2,
+      extensions: [cpp(), oneDark], // 传递给CodeMirror EditorState。创建({扩展})
+    });
+
+
+    // 监听代码语言的选择
+    const handleLangChange = (lang) => {
+      console.log(lang)
+      language = lang
+      if(language === "cpp" || language === "c"){
+        // eslint-disable-next-line no-undef
+        options.extensions = [cpp(), oneDark]
+        if(language === "cpp") options.mode = "text/x-c++src";
+        else if(language === "c") options.mode = "text/x-csrc";
+      }
+      else if(language === "python"){
+        // eslint-disable-next-line no-undef
+        options.extensions = [python(), oneDark]
+        options.mode = "text/x-python";
+      }
+      else if(language === "java"){
+        // eslint-disable-next-line no-undef
+        options.extensions = [java(), oneDark]
+        options.mode = "text/x-java";
+      }
+      else{
+        console.log("unknown language")
+      }
+    }
+
+    return {
+      code,
+      options,
+      language,
+      log: console.log,
+      handleLangChange,
+    };
+  }
+
+}
+</script>
+
+<style scoped>
+  .submitView{
+    width: 800px;
+  }
+  .code{
+    text-align: left;
+    overflow-y: hidden;
+  }
+  .submit{
+    float: right;
+    padding: 10px;
+  }
+
+</style>
