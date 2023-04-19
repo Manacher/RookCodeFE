3a5a9f0653e9bcee29af196f9e0770a3d389f06c
AgarthaSF <1426887306@qq.com>
2023-04-19 12:41:04 +0800

    feat: 日历样式及接口调整完成 --task=1018609  --user=刘宇阳

--- src/views/frontpage/CheckInCalendar.vue
+++ src/views/frontpage/CheckInCalendar.vue
@@ -1,20 +1,65 @@
 <template>
-  <VCalendar color="sky-blue" :attributes="attrs" />
+
+  <VCalendar
+      :attributes="attributes"
+      title-position="right"
+      @dayclick="onDayClickHandler"
+  >
+    <template #day-popover="{attributes }">
+      <div>
+        <ul >
+          <li
+              v-for="{ key, customData } in attributes"
+              :key="key"
+              style="margin-top: 0.7rem; padding-right: 1.5rem "
+          >
+            {{ customData }}
+          </li>
+        </ul>
+      </div>
+    </template>
+  </VCalendar>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-
+import {computed, defineComponent, onMounted, ref} from 'vue';
 export default defineComponent({
   setup() {
-    let attrs = ref([
+
+    let onDayClickHandler = (day: any) => {
+      console.log("checkIn day clicked", day.id)
+    }
+
+    const checkIns = ref([
       {
-        highlight: true,
-        dates: new Date(),
+        description: '2373. 矩阵中的最大局部值',
+        dates: {weekdays: 6},
+        color: 'orange',
+      },
+      {
+        description: '2. 两数相加',
+        dates: {weekdays: 7},
+        color: 'green',
       },
     ]);
+
+    const attributes = computed(() => [
+      ...checkIns.value.map(checkIn => ({
+        dates: checkIn.dates,
+        dot: {
+          color: checkIn.color,
+        },
+        popover: {
+          visibility: 'hover',
+        },
+        customData: checkIn.description,
+      })),
+    ]);
+
     return {
-      attrs,
+      checkIns,
+      attributes,
+      onDayClickHandler,
     };
   },
 });
