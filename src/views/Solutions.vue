199a41e6105d068854564f520fcdba3d086d380e
steven705 <2221903424@qq.com>
2023-04-20 16:29:33 +0800

    --task=1018892 --user=刘志鑫

--- src/views/Solutions.vue
+++ src/views/Solutions.vue
@@ -1,18 +1,7 @@
 <template>
 
 
-  <a-layout
-      style="width: 45%;height: 100%;
-      background-color: white;
-      margin-left: 0.5rem;
-      padding-left: 0.2rem;
-      padding-right: 0.2rem;
-      padding-top: 0.2rem;
-      padding-bottom: 0.2rem;
-      overflow: auto;
-      box-shadow: 0 0 5px 1px #999;
-
-      ">
+  <div class="main_box" :style="myStyle">
 
     <!-- 这一行写搜索和题解   -->
     <a-row>
@@ -27,6 +16,9 @@
         <a-button type="primary"
                   style="width: 100%;background-color: green"
                   @click="writeSolution">
+          <template #icon>
+            <EditOutlined />
+          </template>
           写题解
         </a-button>
       </a-col>
@@ -40,7 +32,7 @@
           font-weight: bold;
       font-family:'宋体',serif;
       font-size: 15px">不限</h4>
-    <div style="height: 100px;overflow: auto;float: left;background-color: white">
+    <div style="padding-bottom: 50px;background-color: white">
         <a-button
             v-for="(item, index) in tag_btn_list"
             :key="index"
@@ -58,20 +50,41 @@
 
     <a-list
         item-layout="horizontal"
-            :data-source="data">
+            :data-source="solution_info_list">
       <template #renderItem="{ item }">
-        <a-list-item>
+        <a-list-item >
           <a-list-item-meta style="text-align: left"
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              :description="item.descrp"
           >
             <template #title>
               <a href="https://www.antdv.com/">{{ item.title }}</a>
             </template>
             <template #avatar>
-              <a-avatar src="https://joeschmoe.io/api/v1/random" />
+              <a-avatar :src="item.avatar" />
             </template>
           </a-list-item-meta>
         </a-list-item>
+
+        <a-tag color="#55acee" style="float: left;margin-left: 2rem;margin-top: 0.3rem">
+          <template #icon>
+            <LikeOutlined />
+          </template>
+          {{item.like_cnt}}
+        </a-tag>
+        <a-tag color="#55acee" style="margin-top: 0.3rem">
+          <template #icon>
+            <UserOutlined />
+          </template>
+          {{item.view_cnt}}
+        </a-tag>
+        <a-tag color="#55acee" style="float: right;margin-right: 2rem;margin-top: 0.3rem">
+          <template #icon>
+            <MessageOutlined />
+          </template>
+          {{item.com_cnt}}
+        </a-tag>
+        <a-divider style="border-color: #7cb305" dashed />
+
       </template>
     </a-list>
 
@@ -81,26 +94,27 @@
     <a-pagination
         v-model:current="current"
         :total="500"
+        style="padding-bottom: 1rem;padding-top: 1rem"
         @change="tableChange">
       <template #itemRender="{ type, originalElement }">
         <a v-if="type === 'prev'">前一页</a>
         <a v-else-if="type === 'next'">后一页</a>
         <component :is="originalElement" v-else></component>
       </template>
+
     </a-pagination>
 
 
 
 
 
-  </a-layout>
+  </div>
 
 </template>
 
 <script lang="ts">
 
 import {defineComponent, onMounted, ref} from 'vue';
-import store from "@/store";
 interface DataItem {
   title: string;
 }
@@ -137,12 +151,17 @@ export default defineComponent({
 
   setup(){
 
-
+    const myStyle=ref({height:""})
     onMounted(() => {
       //初始的时候获取按钮标签
 
+      //获取屏幕高度
+      myStyle.value.height=window.outerHeight-100+"px";
+
 
     })
+
+
 
     /*创建按钮的数组*/
     let tag_btn_list = ref([{tag:'C++',isChoosed:false},
@@ -158,15 +177,32 @@ export default defineComponent({
       {tag:'栈',isChoosed:false},
       {tag:'队列',isChoosed:false}])
 
-    const activeKey = ref(['1']);
-    const customStyle =
-        'background: #f7f7f7;' +
-        'border-radius: 4px;' +
-        'margin-bottom: 24px;' +
-        'border: 0;' +
-        'overflow: hidden'+
-        'width:100%'+
-        'height:500px';
+    /*创建题解的数组*/
+    let solution_info_list=ref([
+        {title:"测试标题1",descrp:"测试内容1哈哈哈哈哈哈哈",
+          avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题2",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题3",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题4",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题5",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题6",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题7",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题8",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题9",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题10",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},
+      {title:"测试标题11",descrp:"测试内容1哈哈哈哈哈哈哈",
+        avatar_src:"https://joeschmoe.io/api/v1/random", like_cnt:0,view_cnt:0,com_cnt:0},])
+
+
 
 
 
@@ -176,6 +212,7 @@ export default defineComponent({
     //搜索题解函数
     const onSearch = (searchValue: string) => {
       alert("搜索")
+      console.log(myStyle)
     };
     /*按钮点击事件*/
     const choosetag=(item:number)=>{
@@ -203,6 +240,10 @@ export default defineComponent({
     const tableChange=(page:number,pageSize:number)=>{
 
       //请求新的题解，做填充
+      //发送的类型为页码加数目
+
+      //将数组清空之后填充
+      
 
     };
 
@@ -211,27 +252,25 @@ export default defineComponent({
 
     return{
 
+      //获取屏幕高度
+      myStyle,
+      //题解的数组
+      solution_info_list,
 
       //测试
       data,
 
       //写题解函数
       writeSolution,
-
       //当前的页码
       current,
       //页码改变的事件
       tableChange,
-
-
       //所有的标签按钮
       tag_btn_list,
       //点击按钮事件
       choosetag,
-
       //下拉选择标签
-      activeKey,
-      customStyle,
 
       //搜索函数
       onSearch,
@@ -248,11 +287,18 @@ export default defineComponent({
 
 <!--占据左侧的位置-->
 <style scoped>
-
-
-
+.main_box{
+  width: 45%;
+  background-color: white;
+  margin-left: 0.5rem;
+  padding-left: 0.2rem;
+  padding-right: 0.2rem;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
+  overflow: auto;
+  box-shadow: 0 0 5px 1px #999;
+}
 .tag_btn_choosed{
   background-color: dodgerblue;
 }
-
 </style>
