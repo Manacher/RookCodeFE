eaf8fdc543974ab46c279dec994350401ff779f1
AgarthaSF <1426887306@qq.com>
2023-04-29 12:39:04 +0800

    feat:调整题解列表样式，添加prettier依赖，--task=1019574 --user=刘宇阳

--- src/views/Problem.vue
+++ src/views/Problem.vue
@@ -1,92 +1,96 @@
 <template>
   <div class="problem">
     <div class="leftStyle">
-
       <a-tabs v-model:activeKey="tabKey" type="card">
         <a-tab-pane key="problem">
-
           <template #tab>
-            <file-search-outlined style="margin-right: 0.5rem"/>
+            <file-search-outlined style="margin-right: 0.5rem" />
             题目描述
           </template>
 
-          <ProblemView :id="Number(params.pro_id)"
-                       style="height: 82vh; overflow-y: hidden;"/>
+          <ProblemView
+            :id="Number(params.pro_id)"
+            style="height: 82vh; overflow-y: hidden; padding-left: 1rem"
+          />
         </a-tab-pane>
 
         <a-tab-pane key="solution">
           <template #tab>
-            <unordered-list-outlined style="margin-right: 0.5rem"/>
+            <unordered-list-outlined style="margin-right: 0.5rem" />
             题解
           </template>
-          <SolutionList v-if="tabKey==='solution'" style="height: 82vh; overflow-y: auto"/>
+          <SolutionList
+            v-if="tabKey === 'solution'"
+            style="height: 82vh; overflow-y: auto"
+          />
         </a-tab-pane>
 
         <a-tab-pane key="submission">
-
           <template #tab>
-            <history-outlined style="margin-right: 0.5rem"/>
+            <history-outlined style="margin-right: 0.5rem" />
             提交记录
           </template>
 
-          <SubmissionList v-if="tabKey==='submission'" style="overflow-y: auto"/>
+          <SubmissionList
+            v-if="tabKey === 'submission'"
+            style="overflow-y: auto; padding-left: 1rem"
+          />
         </a-tab-pane>
-
       </a-tabs>
     </div>
-    <div class="rightStyle" style="position:relative">
-      <Submit @submit="handleSubmit"/>
-      <router-view style="position:absolute; top: 2rem; left: 2rem; right: 2rem;" :key="$route.fullPath"></router-view>
+    <div class="rightStyle" style="position: relative">
+      <Submit @submit="handleSubmit" />
+      <router-view
+        style="position: absolute; top: 0; left: 0; right: 0; height: 92vh"
+        :key="$route.fullPath"
+      ></router-view>
     </div>
-
   </div>
-
-
 </template>
 
 <script lang="ts">
-import {onMounted, ref, shallowRef} from "vue";
+import { onMounted, ref, shallowRef } from "vue";
 import ProblemView from "@/components/Problem/LeftComponent/ProblemView.vue";
 import Submit from "@/components/Problem/RightComponent/Submit.vue";
-import SolutionList from "@/components/Problem/LeftComponent/questionSolutions.vue"
+import SolutionList from "@/components/Problem/LeftComponent/questionSolutions.vue";
 import SubmissionList from "@/components/Problem/LeftComponent/SubmissionList.vue";
-import {useRoute, useRouter} from 'vue-router';
+import { useRoute, useRouter } from "vue-router";
 
 export default {
   name: "Problem",
-  components: {ProblemView, Submit, SolutionList, SubmissionList},
+  components: { ProblemView, Submit, SolutionList, SubmissionList },
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   setup() {
     // 获取路由参数
-    const {query, params} = useRoute()
+    const { query, params } = useRoute();
     // 菜单栏key
-    const tabKey = ref('problem');
-
+    const tabKey = ref("problem");
 
     // 监听题目提交
     const handleSubmit = (val: number) => {
       //TODO: Do Nothing
-      console.log(val)
-    }
+      console.log(val);
+    };
 
     let modifyStyle = () => {
-      let tabNav = document.getElementsByClassName("ant-tabs-nav-wrap")[0] as HTMLElement
-      tabNav.style.background = "#f7f7f7"
-    }
+      let tabNav = document.getElementsByClassName(
+        "ant-tabs-nav-wrap"
+      )[0] as HTMLElement;
+      tabNav.style.background = "#f7f7f7";
+    };
 
     onMounted(() => {
-      modifyStyle()
-    })
+      modifyStyle();
+    });
 
     return {
       tabKey,
       query,
       params,
       handleSubmit,
-
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -123,9 +127,8 @@ export default {
   background: #f7f7f7;
 }
 
-
 .leftStyle > .ant-tabs-card .ant-tabs-content > .ant-tabs-tabpane {
-  padding-left: 1rem;
+  /*padding-left: 1rem;*/
   /*padding-right: 1rem;*/
   background: #fff;
 }
@@ -133,6 +136,4 @@ export default {
 .leftStyle > .ant-tabs-card > .ant-tabs-nav::before {
   display: none;
 }
-
-
 </style>
