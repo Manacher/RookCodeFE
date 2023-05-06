ed751cfcc7271dba229ceb4430ef97b0169951b5
AgarthaSF <1426887306@qq.com>
2023-05-06 10:27:52 +0800

    feat:优化通过列表和题解列表样式 --story=1017963 --user=刘宇阳

--- src/components/UserPage/UserPublishList.vue
+++ src/components/UserPage/UserPublishList.vue
@@ -1,28 +1,31 @@
 <template>
-  <a-list item-layout="horizontal"
-          id="publish-list"
-          size="large"
-          :pagination="pagination"
-          :data-source="publishList"
-          :loading="loading"
-          style="width: 100%">
-
-    <template #renderItem="{item, index}">
-
-      <a-list-item key="item.title"
-                   @click="onTitleClicked(item.question_id, item.id)"
-                   :style="{'background-color':(index%2 === 1 ? '#fff' : '#f7f7f8'),
-                   'border-radius': (index%2 === 1 ? '0' : '0.35rem')}">
-
+  <a-list
+    item-layout="horizontal"
+    id="publish-list"
+    size="large"
+    :pagination="pagination"
+    :data-source="publishList"
+    :loading="loading"
+    style="width: 100%"
+  >
+    <template #renderItem="{ item, index }">
+      <a-list-item
+        key="item.title"
+        @click="onTitleClicked(item.question_id, item.id)"
+        :style="{
+          'background-color': index % 2 === 1 ? '#fff' : '#f7f7f8',
+          'border-radius': index % 2 === 1 ? '0' : '0.35rem',
+          border: 'none',
+        }"
+      >
         <template #actions>
-
           <a-space direction="horizontal">
             <div class="publish-list-action-item" style="width: 4rem">
-              <like-outlined style="margin-right: 0.3rem;"/>
+              <like-outlined style="margin-right: 0.3rem" />
               {{ item.like }}
             </div>
             <div class="publish-list-action-item" style="width: 4rem">
-              <eye-outlined style="margin-right: 0.3rem;"/>
+              <eye-outlined style="margin-right: 0.3rem" />
               {{ item.view }}
             </div>
             <div class="publish-list-action-item" style="width: 4rem">
@@ -30,25 +33,20 @@
             </div>
             <a @click="onEditClicked(item.question_id, item.id)">编辑</a>
           </a-space>
-
-
         </template>
         <div>
           <a style="color: #262626">{{ item.title }}</a>
         </div>
       </a-list-item>
-
     </template>
   </a-list>
-
 </template>
 
 <script lang="ts">
-
-import {ref} from "vue";
-import {getUserPublishList} from "@/components/UserPage/userPageHttp";
+import { ref } from "vue";
+import { getUserPublishList } from "@/components/UserPage/userPageHttp";
 import moment from "moment/moment";
-import {message} from "ant-design-vue";
+import { message } from "ant-design-vue";
 
 interface publishListData {
   title: string;
@@ -69,24 +67,23 @@ interface publishRespData {
   question_id: number;
 }
 
-let loading = ref(false)
+let loading = ref(false);
 
 export default {
-  props: ['account'],
+  props: ["account"],
   name: "UserPublishList",
   setup(props: any, context: any) {
-
     let pagination = ref({
       current: 1,
       pageSize: 15,
       total: 15,
       showSizeChanger: false,
       onChange: (page: number) => {
-        handleQuery(page)
-      }
+        handleQuery(page);
+      },
     });
 
-    let publishList = ref<publishListData[]>([])
+    let publishList = ref<publishListData[]>([]);
 
     let handleQuery = (page: number) => {
       loading.value = true;
@@ -94,44 +91,48 @@ export default {
         if (res.success) {
           let data = res.data;
 
-          console.log("publish data", data.findSolutionRespList)
+          console.log("publish data", data.findSolutionRespList);
 
           pagination.value.total = data.total_page * pagination.value.pageSize;
           pagination.value.current = page;
 
-          publishList.value = []
-          moment.locale('zh-cn');
+          publishList.value = [];
+          moment.locale("zh-cn");
 
-          let list = data.findSolutionRespList
+          let list = data.findSolutionRespList;
 
           list.forEach((val: publishRespData) => {
             publishList.value.push({
-              title: val.question_id + '. ' + val.question_title + ' - ' + val.solution_title,
+              title:
+                val.question_id +
+                ". " +
+                val.question_title +
+                " - " +
+                val.solution_title,
               like: val.like,
               view: val.view,
               date: moment(val.date).fromNow(),
               id: val.id,
               question_id: val.question_id,
-            })
-          })
+            });
+          });
           loading.value = false;
         } else {
-          message.error(res.message)
+          message.error(res.message);
           loading.value = false;
         }
-      })
-    }
+      });
+    };
 
-    handleQuery(pagination.value.current)
+    handleQuery(pagination.value.current);
 
-    let onTitleClicked = (question_id:number, solution_id: number) => {
-      window.open(`/problems/${question_id}/solution/${solution_id}`)
-    }
+    let onTitleClicked = (question_id: number, solution_id: number) => {
+      window.open(`/problems/${question_id}/solution/${solution_id}`);
+    };
 
-    let onEditClicked = (question_id:number, solution_id: number) => {
-      window.open(`/problems/${question_id}/edit-solution/${solution_id}`)
-    }
-
+    let onEditClicked = (question_id: number, solution_id: number) => {
+      window.open(`/problems/${question_id}/edit-solution/${solution_id}`);
+    };
 
     return {
       pagination,
@@ -141,20 +142,22 @@ export default {
       onEditClicked,
     };
   },
-}
+};
 </script>
 
 <style scoped>
-
 .publish-list-action-item {
   margin-right: 0.5rem;
-  /*margin-left: 0.5rem;*/
   text-align: right;
 }
-
 
 #publish-list .ant-list-item {
   cursor: pointer;
 }
 
+#publish-list .ant-list-item:hover {
+  background-color: #eff0f2 !important;
+  transition: background-color 0.2s ease-in-out !important;
+  border-radius: 0.35rem !important;
+}
 </style>
--- src/components/UserPage/UserRecentPassList.vue
+++ src/components/UserPage/UserRecentPassList.vue
@@ -1,110 +1,107 @@
 <template>
-  <a-list item-layout="horizontal"
-          id="pass-list"
-          size="large"
-          :pagination="pagination"
-          :data-source="passList"
-          :loading="loading"
-          @change="handleListChange"
-          style="width: 100%">
-
-    <template #renderItem="{item, index}">
-      <a-list-item key="item.title"
-                   @click="onTitleClicked(item.id)"
-                   :style="{'background-color':(index%2 === 1 ? '#fff' : '#f7f7f8'),
-                   'border-radius': (index%2 === 1 ? '0' : '0.35rem')}">
+  <a-list
+    item-layout="horizontal"
+    id="pass-list"
+    size="large"
+    :pagination="pagination"
+    :data-source="passList"
+    :loading="loading"
+    @change="handleListChange"
+    style="width: 100%"
+  >
+    <template #renderItem="{ item, index }">
+      <a-list-item
+        key="item.title"
+        @click="onTitleClicked(item.id)"
+        :style="{
+          'background-color': index % 2 === 1 ? '#fff' : '#f7f7f8',
+          'border-radius': index % 2 === 1 ? '0' : '0.35rem',
+          border: 'none',
+        }"
+      >
         <template #actions>
           <div>
-
             <a-space direction="horizontal">
-              <div >提交时间</div>
+              <div>提交时间</div>
 
-              <div style="width: 4rem;text-align: right">{{ item.date }}</div>
+              <div style="width: 4rem; text-align: right">{{ item.date }}</div>
             </a-space>
-
-
           </div>
-
         </template>
         <div>
           <a style="color: #262626">{{ item.title }}</a>
         </div>
-
       </a-list-item>
     </template>
   </a-list>
-
 </template>
 
 <script lang="ts">
+import { getUserRecentPassList } from "@/components/UserPage/userPageHttp";
+import { ref } from "vue";
+import { message } from "ant-design-vue";
+import moment from "moment";
 
-import {getUserRecentPassList} from "@/components/UserPage/userPageHttp";
-import {ref} from "vue";
-import {message} from "ant-design-vue";
-import moment from 'moment';
-
-interface passListData{
+interface passListData {
   title: string;
   date: string;
   id: number;
   question_id?: number;
 }
 
-let passList = ref<passListData[]>([])
-let loading = ref(false)
+let passList = ref<passListData[]>([]);
+let loading = ref(false);
 
 export default {
-  props: ['account'],
+  props: ["account"],
   name: "UserRecentPassList",
   setup(props: any, context: any) {
-
-
     let pagination = ref({
       current: 1,
       pageSize: 15,
       total: 15,
       showSizeChanger: false,
       onChange: (page: number) => {
-        handleQuery(page)
-      }
+        handleQuery(page);
+      },
     });
 
     let onTitleClicked = (id: number) => {
-      window.open(`/submissions/detail/${id}`)
-    }
+      window.open(`/submissions/detail/${id}`);
+    };
 
     let handleQuery = (page: number) => {
       loading.value = true;
       getUserRecentPassList(props.account, page).then((res: any) => {
-        console.log("recent pass list", res)
-        if(res.success){
+        console.log("recent pass list", res);
+        if (res.success) {
           let data = res.data;
 
           pagination.value.total = data.total_page * pagination.value.pageSize;
           pagination.value.current = page;
 
-          passList.value = []
+          passList.value = [];
 
-          moment.locale('zh-cn');
-          data.infoRespList.forEach((val : passListData) => {
+          moment.locale("zh-cn");
+          data.infoRespList.forEach((val: passListData) => {
             passList.value.push({
-              title: val.question_id + '. ' + val.title,
+              title: val.question_id + ". " + val.title,
               date: moment(val.date).fromNow(),
               id: val.id,
-            })
-          })
+            });
+          });
           loading.value = false;
-        }else{
-          message.error(res.message)
+        } else {
+          message.error(res.message);
           loading.value = false;
         }
-      })
-    }
+      });
+    };
 
-    handleQuery(pagination.value.current)
+    handleQuery(pagination.value.current);
 
     let handleListChange = (pagination: any) => {
-      console.log("handleListChange", pagination)
+      console.log("handleListChange", pagination);
       handleQuery(pagination.current);
     };
 
@@ -116,13 +113,17 @@ export default {
       handleListChange,
     };
   },
-}
+};
 </script>
 
 <style scoped>
-
 #pass-list .ant-list-item {
   cursor: pointer;
 }
 
+#pass-list .ant-list-item:hover {
+  background-color: #eff0f2 !important;
+  transition: background-color 0.2s ease-in-out !important;
+  border-radius: 0.35rem !important;
+}
 </style>
