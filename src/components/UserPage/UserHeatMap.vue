dfe0ce3a0a0d67042cf9c43ee97776633051a918
AgarthaSF <1426887306@qq.com>
2023-05-06 10:28:38 +0800

    feat:修复heatmap最大值的小问题，现令最大值为用户提交的最大次数+1，避免提交次数全为0却全为绿的情况发生 --story=1017963 --user=刘宇阳

--- src/components/UserPage/UserHeatMap.vue
+++ src/components/UserPage/UserHeatMap.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="heatmap-main">
-
     <div class="text-area">
       <div>
         过去一年共提交
-        <span style="font-size: 1.1rem; color: #262626; font-weight: bold">{{ totalNum }}</span>
+        <span style="font-size: 1.1rem; color: #262626; font-weight: bold">{{
+          totalNum
+        }}</span>
         次
       </div>
 
       <div style="margin-left: auto; color: #8a8a8e">
         累积提交天数:
         <span style="color: #5c5c5c">{{ totalDay }}</span>
-        <a-select v-model:value="yearSelected" style="width: 6.5rem; margin-left: 1.5rem;">
+        <a-select
+          v-model:value="yearSelected"
+          style="width: 6.5rem; margin-left: 1.5rem"
+        >
           <a-select-option value="lastYear">过去一年</a-select-option>
         </a-select>
       </div>
-
     </div>
 
     <div class="heatmap-container">
@@ -25,63 +28,78 @@
 </template>
 
 <script lang="ts">
-
-import * as echarts from 'echarts';
-import {onMounted, ref} from "vue";
-import {getUserYearSubmit} from "@/components/UserPage/userPageHttp";
-import {message} from "ant-design-vue";
-import {processUserHeatmapData} from "@/components/UserPage/userPageUtil";
-
+import * as echarts from "echarts";
+import { onMounted, ref } from "vue";
+import { getUserYearSubmit } from "@/components/UserPage/userPageHttp";
+import { message } from "ant-design-vue";
+import { processUserHeatmapData } from "@/components/UserPage/userPageUtil";
 
 export default {
-  props: ['account'],
+  props: ["account"],
   name: "user-heat-map",
 
   setup(props: any, context: any) {
-
     let totalNum = ref(0);
     let totalDay = ref(0);
-    let yearSelected = ref('lastYear');
+    let yearSelected = ref("lastYear");
 
     let changeSelectorStyle = () => {
-      let selector = document.getElementsByClassName('ant-select-selector')[0] as HTMLElement;
-      selector.style.border = 'none';
-      selector.style.background = '#f2f3f4';
-      selector.style.borderRadius = '0.3rem';
-      selector.style.color = '#595959';
-    }
+      let selector = document.getElementsByClassName(
+        "ant-select-selector"
+      )[0] as HTMLElement;
+      selector.style.border = "none";
+      selector.style.background = "#f2f3f4";
+      selector.style.borderRadius = "0.3rem";
+      selector.style.color = "#595959";
+    };
 
     let init = () => {
       getUserYearSubmit(props.account).then((res: any) => {
         if (res.success) {
           let processRes = processUserHeatmapData(res.data);
-          let dataList = processRes[0]
-          let maxSubmit = processRes[1] as number
-          totalNum.value = processRes[2] as number
-          totalDay.value = processRes[3] as number
+          let dataList = processRes[0];
+          let maxSubmit = processRes[1] as number;
+          totalNum.value = processRes[2] as number;
+          totalDay.value = processRes[3] as number;
 
           // console.log("dataList", dataList)
 
           let today = Number(echarts.number.parseDate(new Date()));
           let lastYearDay = today - 3600 * 24 * 1000 * 364;
-          let todayString = echarts.format.formatTime('yyyy-MM-dd', today)
-          let lastYearDayString = echarts.format.formatTime('yyyy-MM-dd', lastYearDay)
-          renderChart(todayString, lastYearDayString, dataList, maxSubmit)
+          let todayString = echarts.format.formatTime("yyyy-MM-dd", today);
+          let lastYearDayString = echarts.format.formatTime(
+            "yyyy-MM-dd",
+            lastYearDay
+          );
+          renderChart(todayString, lastYearDayString, dataList, maxSubmit);
         } else {
-          message.error(res.message)
+          message.error(res.message);
         }
+      });
+    };
 
-      })
-    }
-
-    let renderChart = (startDate: string, endDate: string, data: any, maxSubmit: number) => {
-      let myChart = echarts.init(document.getElementById('heatmap') as HTMLElement);
+    let renderChart = (
+      startDate: string,
+      endDate: string,
+      data: any,
+      maxSubmit: number
+    ) => {
+      let myChart = echarts.init(
+        document.getElementById("heatmap") as HTMLElement
+      );
       let option = {
         visualMap: {
           min: 0,
-          max: maxSubmit,
+          max: maxSubmit + 1,
           inRange: {
-            color: ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196027', '#175b2f']
+            color: [
+              "#ebedf0",
+              "#c6e48b",
+              "#7bc96f",
+              "#239a3b",
+              "#196027",
+              "#175b2f",
+            ],
           },
           show: false,
         },
@@ -89,70 +107,67 @@ export default {
         // 显示提交数量与日期
         tooltip: {
           formatter: function (p: any) {
-            let format = echarts.format.formatTime('yyyy-MM-dd', p.data[0]);
-            let suffix = p.data[1] === 0 ? '没有提交，' : p.data[1] + '个提交，'
+            let format = echarts.format.formatTime("yyyy-MM-dd", p.data[0]);
+            let suffix =
+              p.data[1] === 0 ? "没有提交，" : p.data[1] + "个提交，";
             return suffix + format;
           },
-          backgroundColor: '#2a2a2a',
+          backgroundColor: "#2a2a2a",
           textStyle: {
-            color: '#ffffff'
-          }
-
+            color: "#ffffff",
+          },
         },
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         calendar: {
           top: 0,
           left: 5,
           cellSize: [12, 12],
           range: [startDate, endDate],
           itemStyle: {
-            borderColor: '#fff',
-            borderWidth: 3
+            borderColor: "#fff",
+            borderWidth: 3,
           },
           splitLine: {
-            show: false
+            show: false,
           },
-          yearLabel: {show: false},
+          yearLabel: { show: false },
           //x轴坐标
           monthLabel: {
-            position: 'bottom',
+            position: "bottom",
             textStyle: {
-              color: '#afb4bd'
+              color: "#afb4bd",
             },
-            padding: [3, 0, 0, 0]
+            padding: [3, 0, 0, 0],
           },
           //y轴坐标
           dayLabel: {
             show: false,
-          }
+          },
         },
         series: {
-          type: 'heatmap',
-          coordinateSystem: 'calendar',
+          type: "heatmap",
+          coordinateSystem: "calendar",
           data: data,
         },
       };
       myChart.setOption(option);
-    }
+    };
 
     onMounted(() => {
       init();
       changeSelectorStyle();
-    })
+    });
 
     return {
       totalNum,
       totalDay,
       yearSelected,
-    }
-
-  }
-
-}
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 .heatmap-main {
   background-color: white;
   border-radius: 1rem;
@@ -174,5 +189,4 @@ export default {
   overflow-x: auto;
   display: inline-block;
 }
-
 </style>
